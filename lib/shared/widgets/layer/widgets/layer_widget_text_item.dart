5d0994100d48190919c201fc663deeec1a862188
dola <mohamed.adeltawfiq@roaa.tech>
2025-05-11 21:26:24 +0300

    make text edtor layer accept edit widgets

--- lib/shared/widgets/layer/widgets/layer_widget_text_item.dart
+++ lib/shared/widgets/layer/widgets/layer_widget_text_item.dart
@@ -54,58 +54,60 @@ class LayerWidgetTextItem extends StatelessWidget {
               clipBehavior: Clip.none,
               children: [
                 InkWell(
-                // onTap: (){onEdit();},
-                  child: Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      //  borderRadius: BorderRadius.circular(8),
-                      border:value ? Border.all(color: Colors.red, width: 1) : null,
-                    ),
-                    child: RoundedBackgroundText(
-                      onHitTestResult: (hasHit) {
-                        // Update hit detection and cursor visibility state.
-                        if (layer.hit != hasHit ||
-                            showMoveCursor.value != hasHit) {
+                 onTap: (){onEdit();},
+                  child: CustomPaint(
+                    painter: value ?_DashedBorderPainter() : null,
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: RoundedBackgroundText(
+                        onHitTestResult: (hasHit) {
+                          // Update hit detection and cursor visibility state.
+                          if (layer.hit != hasHit ||
+                              showMoveCursor.value != hasHit) {
+                            layer.hit = hasHit;
+                            showMoveCursor.value = hasHit;
+                          }
                           layer.hit = hasHit;
-                          showMoveCursor.value = hasHit;
-                        }
-                        layer.hit = hasHit;
-                        onHitChanged(hasHit);
-                      },
-                      layer.text.toString(),
-                      backgroundColor: layer.background,
-                      textAlign: layer.align,
-                      style: layer.textStyle?.copyWith(
-                            fontSize: style.fontSize,
-                            fontWeight: style.fontWeight,
-                            color: style.color,
-                            fontFamily: style.fontFamily,
-                          ) ??
-                          style,
+                          onHitChanged(hasHit);
+                        },
+                        layer.text.toString(),
+                        backgroundColor: layer.background,
+                        textAlign: layer.align,
+                        style: layer.textStyle?.copyWith(
+                              fontSize: style.fontSize,
+                              fontWeight: style.fontWeight,
+                              color: style.color,
+                              fontFamily: style.fontFamily,
+                            ) ??
+                            style,
+                      ),
                     ),
                   ),
                 ),
                 // Edit icon at top-left corner
+                // if (value)
+                //   Positioned(
+                //     top: -10,
+                //     left: -10,
+                //     child: InkWell(
+                //       onTap: (){
+                //         onEdit();
+                //       },
+                //       child: Container(
+                //           decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+                //           child: const Icon(Icons.edit, color: Colors.green)),
+                //     ),
+                //   ),
+                // Delete icon at top-right corner
                 if (value)
                   Positioned(
                     top: -10,
                     left: -10,
                     child: InkWell(
-                      onTap: (){
-                        onEdit();
-                      },
-                      child: Container(
+                      child:  Container(
+                        padding: EdgeInsets.all(5),
                           decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-                          child: const Icon(Icons.edit, color: Colors.green)),
-                    ),
-                  ),
-                // Delete icon at top-right corner
-                if (value)
-                  Positioned(
-                    top: -10,
-                    right: -10,
-                    child: InkWell(
-                      child: const Icon(Icons.delete, color: Colors.red),
+                          child: Image.asset("assets/images/remove.png",width: 20,height: 20,)),
                       onTap: () => onRemove(),
                     ),
                   ),
@@ -114,4 +116,55 @@ class LayerWidgetTextItem extends StatelessWidget {
           );
         });
   }
+}
+
+
+
+class _DashedBorderPainter extends CustomPainter {
+  final double dashWidth;
+  final double dashSpace;
+  final Color color;
+  final double strokeWidth;
+
+  _DashedBorderPainter({
+    this.dashWidth = 5,
+    this.dashSpace = 3,
+    this.color = Colors.black,
+    this.strokeWidth = 1,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth..strokeJoin = StrokeJoin.round
+      ..style = PaintingStyle.stroke;
+
+    // Top side
+    _drawDashedLine(canvas, Offset(0, 0), Offset(size.width, 0), paint);
+    // Right side
+    _drawDashedLine(canvas, Offset(size.width, 0), Offset(size.width, size.height),
+        paint);
+    // Bottom side
+    _drawDashedLine(canvas, Offset(size.width , size.height), Offset(0, size.height), paint);
+    // Left side
+    _drawDashedLine(canvas, Offset(0, size.height), const Offset(0, 0), paint);
+  }
+
+  void _drawDashedLine(Canvas canvas, Offset start, Offset end, Paint paint) {
+    final totalLength = (end - start).distance;
+    final dashCount = (totalLength / (dashWidth + dashSpace)).floor();
+    final dx = (end.dx - start.dx) / totalLength;
+    final dy = (end.dy - start.dy) / totalLength;
+
+    for (int i = 0; i < dashCount; ++i) {
+      final currentLength = i * (dashWidth + dashSpace);
+      final from = Offset(start.dx + dx * currentLength, start.dy + dy * currentLength);
+      final to = Offset(from.dx + dx * dashWidth, from.dy + dy * dashWidth);
+      canvas.drawLine(from, to, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
